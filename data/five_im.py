485bcf41b83580dfb05fd9613805e793ac0f08a8
a-blokhin <blokhin.aa17@physics.msu.ru>
2021-06-28 23:00:20 +0500

    улучшение бота

---
+++ data/five_im.py
@@ -0,0 +1,5 @@
+https://sr.gallerix.ru/_EX/280359500/413874770.jpg
+https://yt3.ggpht.com/a/AATXAJwRaOxIawDqNVJgdXFCkmBglWxvSsYGf08TZ3LP=s900-c-k-c0xffffffff-no-rj-mo
+https://outofthisworldx.files.wordpress.com/2016/08/scream.jpg?w=736
+https://picasso-picasso.ru/wp-content/uploads/17ku.jpg
+https://gallerix.ru/fullpic/69343788310592efca5bcdeab33e8637/
