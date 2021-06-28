9034224788dd3a66fd9ca2425383f3405ccd514f
a-blokhin <86594714+a-blokhin@users.noreply.github.com>
2021-06-28 23:46:45 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,3 +7,4 @@ from loader import dp
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Привет, {message.from_user.full_name}!")
+    await message.answer("Бот преднзначен для переноса стилей между картинками и/или стикерами, эта функция включается командой /style_transfer . Если вы хотите найти подходящие для этой задачи картинки, то подбор картинок реализован командой /pict . Картинки в style-transfer отправляйте пожалуйста по одной, если отправляете стикеры, то без прозрачного фона, то есть прямоугольные.")
